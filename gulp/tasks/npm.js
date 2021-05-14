d83a1f5b6a1a9872120b172f953fc1fee8161fd3
matiastang <18380449615@163.com>
2021-05-14 15:02:59 +0800

    添加push npm package to npm 之前,使用nrm 切换npm源到npm

--- gulp/tasks/npm.js
+++ gulp/tasks/npm.js
@@ -2,7 +2,7 @@
  * @Author: tangdaoyong
  * @Date: 2021-05-14 14:51:10
  * @LastEditors: tangdaoyong
- * @LastEditTime: 2021-05-14 14:53:50
+ * @LastEditTime: 2021-05-14 15:01:06
  * @Description: npm 相关
  */
 /**
@@ -11,10 +11,12 @@
  * @param {*} plugins 
  * @param {*} cb 
  */
- const npmPackagePush = async function (gulp, plugins, cb) {
+const npmPackagePush = async function (gulp, plugins, cb) {
+    let chooseNPM = 'nrm use npm ';// 切换源到npm(使用nrm)
     let command = 'npm publish'; // 提交npm
     // 执行指令
     await plugins.shell.exec(`
+        ${chooseNPM}
         ${command}
     `, (error, stdout, stderr) => {
         if (error) {
